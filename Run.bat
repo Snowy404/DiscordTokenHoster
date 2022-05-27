75d9b19fd0fd24c08556d528fa5dfb811fab691a
Snowy <65083757+Snowy404@users.noreply.github.com>
2022-05-27 18:32:05 +0100

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -3,5 +3,5 @@ echo Successfully hosting all tokens!
 echo The script will be refreshed every 10 minutes to ensure all accounts stay logged in.
 :loop
 python onliner.py
-timeout /t 600
+timeout /t 600 /nobreak
 goto loop
